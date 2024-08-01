ca14c883cce6bbbf95022ed8747315b8537555ad
wjqzlhhhh <62372158+wjqzlhhhh@users.noreply.github.com>
2024-08-01 14:43:05 +0800

    Delete peidi-supplier-portal directory

--- peidi-supplier-portal/src/main/java/com/cyanrocks/wms/dao/SalesOrderDao.java
+++
@@ -1,7 +0,0 @@
-package com.cyanrocks.wms.dao;
-
-import com.cyanrocks.wms.entity.SalesOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalesOrderDao extends JpaRepository<SalesOrder,Integer> {
-}
